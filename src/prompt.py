52516470064bca84c02f570ee303adaebf7a08c1
Sanjiv KS <32534275+Roniwatson4222@users.noreply.github.com>
2026-04-12 21:04:00 +0530

    Update prompt.py

--- src/prompt.py
+++ src/prompt.py
@@ -1,4 +1,4 @@
-system_prompt = """You are an assistant for question-answering tasks.
+system_prompt = """You are a medical assistant for question-answer tasks.
 Use the following retrieved documents to answer the question.
 If you don't know the answer, say you don't know.
 Use three sentences maximum and keep the answer concise.
